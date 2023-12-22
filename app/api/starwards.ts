863f72bbcc1ee12f5c1b3ce4370ce68cb6b636b0
tami <tamiortami@gmail.com>
2023-12-22 15:47:39 +0900

    fix

--- app/api/starwards.ts
+++ app/api/starwards.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://swapi.dev/api";
+const baseUrl = "https://swapi.dev/api";
 
 export const getPerson = () => {
   return axios.get(`${baseUrl}/people`).then((res) => res.data);
