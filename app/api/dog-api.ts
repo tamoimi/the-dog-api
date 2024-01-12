5c4990ce4f6a4ceffde7b76f55ee0588ec4828ab
tami <tamiortami@gmail.com>
2024-01-12 17:22:23 +0900

    feat: api

---
+++ app/api/dog-api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const baseUrl = "https://api.thedogapi.com/v1";
+
+const baseUrlV2 = "https://dogapi.dog/api/v2";
+
+// export const getDogs = () => {
+//   return axios.get(`${baseUrl}/images/BkIEhN3pG`).then((res) => res.data);
+// };
+
+// dog-api
+export const getDogData = () => {
+  return axios.get(`${baseUrlV2}/breeds`).then((res) => res.data);
+};
+
