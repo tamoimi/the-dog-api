a9753b9a0afe9d0f84df6838b6301f48a94cf89e
tami <tamiortami@gmail.com>
2024-02-22 09:15:28 +0900

    style: round color

--- app/components/FunFact.tsx
+++ app/components/FunFact.tsx
@@ -30,14 +30,24 @@ const FunFact = () => {
                 src="/docs/images/people/profile-picture-3.jpg"
                 alt="Tami image"
               /> */}
-              <div className="w-8 h-8 rounded-full bg-indigo-500 text-center font-bold text-zinc-50">T</div>
+              <div className="w-8 h-8 rounded-full bg-blue-400 text-center font-bold text-zinc-50">
+                T
+              </div>
               <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                 <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                  <span className="text-sm font-semibold text-gray-900 dark:text-white">Tami Kim</span>
-                  <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11 :46</span>
+                  <span className="text-sm font-semibold text-gray-900 dark:text-white">
+                    Tami Kim
+                  </span>
+                  <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                    11 :46
+                  </span>
                 </div>
-                <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{funFactData}</p>
-                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
+                <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
+                  {funFactData}
+                </p>
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Delivered
+                </span>
               </div>
             </div>
           </div>
