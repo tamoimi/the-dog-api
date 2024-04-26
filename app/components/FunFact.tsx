df214ac7defe39cf9d87aaf292720639a3fda3ff
tami <tamiortami@gmail.com>
2024-04-26 11:16:58 +0900

    feat: date setting

--- app/components/FunFact.tsx
+++ app/components/FunFact.tsx
@@ -1,21 +1,23 @@
 import useSWR from "swr";
 import { getFunFact } from "../api/dog-api";
 import Loading from "../loading";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
 const FunFact = () => {
+  
   const { data: factData, isLoading: factLoading } = useSWR("/api/dog-fun-facts", getFunFact, {
     refreshInterval: 86400000, // refresh once a day
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
 
-  console.log("factData", factData);
   const funFactData =
     factData && factData.data && factData.data[0] && factData.data[0].attributes
       ? factData.data[0].attributes.body
       : null;
 
-  console.log("funFactData", funFactData);
+  const nowTime = moment().format("YYYY-MM-DD");
 
   return (
     <>
@@ -25,11 +27,6 @@ const FunFact = () => {
         <>
           <div className="grid md:grid-cols-2 sm:grid-cols-1">
             <div className="flex items-start gap-2.5">
-              {/* <Image
-                className="w-8 h-8 rounded-full"
-                src="/docs/images/people/profile-picture-3.jpg"
-                alt="Tami image"
-              /> */}
               <div className="w-8 h-8 rounded-full bg-blue-400 text-center font-bold text-zinc-50">
                 T
               </div>
@@ -39,7 +36,7 @@ const FunFact = () => {
                     Tami Kim
                   </span>
                   <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                    11 :46
+                    {nowTime}
                   </span>
                 </div>
                 <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
